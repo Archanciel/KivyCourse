6de35720899c5255aebc21732dfa921e4fee39f8
Archanciel <jp.schnyder@gmail.com>
2021-12-29 20:02:03 +0100

    setting label font

--- mykivywidget.py
+++ mykivywidget.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
 
 class MyGridLayout(GridLayout):
+	labelTxt = StringProperty('0')
+	
 	def __init__(self, **kvarg):
 		super().__init__(**kvarg)
+		
+		self.clickNb = 0
 
+	def handleClick(self):
+		self.clickNb += 1
+		self.labelTxt = str(self.clickNb) 	
 	
 class MyKivyWidgetApp(App):
 	def build(self):
