e64d58bbbe8496a7f3d498f7a031c5f80d855f5a
Archanciel <jp.schnyder@gmail.com>
2021-12-30 17:56:38 +0100

    new additions

--- mykivywidget.py
+++ mykivywidget.py
@@ -1,18 +1,38 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
 
 class MyGridLayout(GridLayout):
 	labelTxt = StringProperty('0')
+	clickDisabled = BooleanProperty(True)
+	switchActive = BooleanProperty(False)
+	sliderLabelTxt = StringProperty('0')
+	sliderDisabled = BooleanProperty(True)
 	
 	def __init__(self, **kvarg):
 		super().__init__(**kvarg)
 		
 		self.clickNb = 0
+		self.clickNumberActive = False
 
 	def handleClick(self):
 		self.clickNb += 1
-		self.labelTxt = str(self.clickNb) 	
+		self.labelTxt = str(self.clickNb) 
+		
+	def handleToggle(self, widget):
+		if widget.state == 'normal':
+			widget.text = 'Off'
+			self.clickDisabled = True
+		else:	
+			widget.text = 'On'
+			self.clickDisabled = False
+
+	def handleSwitch(self, widget):
+		self.sliderDisabled = not widget.active
+			
+	def handleSlider(self, widget):
+		self.sliderLabelTxt = str(int(widget.value))
 	
 class MyKivyWidgetApp(App):
 	def build(self):
